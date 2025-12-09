00042ae67b4604336aa2cd15983cd978f2543185
Deku394 <almuhametovvadim2005@gmail.com>
2025-12-09 20:08:46 +0300

    Update keyboards.py

--- bot_test/keyboards.py
+++ bot_test/keyboards.py
@@ -4,8 +4,8 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 start_kb = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(text="Консультация", callback_data="consultation")],
     [InlineKeyboardButton(text="Бесплатные курсы", callback_data="free_courses")],
-    [InlineKeyboardButton(text="Массфолл для блога", url='https://t.me/ZackFosteroverkill')],
-    [InlineKeyboardButton(text="Мой канал в тг", url='https://t.me/rudnyov')],
+    [InlineKeyboardButton(text="Массфолл для блога", url='https://t.me/+mX695lhKCAxhOGVi')],
+    [InlineKeyboardButton(text="Мой канал в тг", url='https://t.me/+mX695lhKCAxhOGVi')],
     [InlineKeyboardButton(text="Про продвижение", callback_data="about_instagram")],
 ]
 )
@@ -18,9 +18,9 @@ consultation_kb = InlineKeyboardMarkup(
 
 free_courses_kb = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text='Курс по хэштегам', url='https://t.me/rudnyov')],
-        [InlineKeyboardButton(text='Система вечного контента', url='https://t.me/rudnyov')],
-        [InlineKeyboardButton(text='Обучение по отметкам', url='https://t.me/rudnyov')],
+        [InlineKeyboardButton(text='Курс по хэштегам', url='https://t.me/+mX695lhKCAxhOGVi')],
+        [InlineKeyboardButton(text='Система вечного контента', url='https://t.me/+mX695lhKCAxhOGVi')],
+        [InlineKeyboardButton(text='Обучение по отметкам', url='https://t.me/+mX695lhKCAxhOGVi')],
         [InlineKeyboardButton(text='Назад в главное меню', callback_data='back_menu')]
     ]
 )
@@ -29,8 +29,7 @@ about_instagram_kb = InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text='Рабочая тетрадь по сторис', callback_data='story_notebook')],
         [InlineKeyboardButton(text='Марафон по продвижению', callback_data='inst_marathon')],
-        [InlineKeyboardButton(text='Канал "штучки для рилс"', url='https://t.me/rudnyov')],
-        [InlineKeyboardButton(text='Задать вопрос', callback_data='ask_question')],
+        [InlineKeyboardButton(text='Канал "штучки для рилс"', url='https://t.me/+mX695lhKCAxhOGVi')],
         [InlineKeyboardButton(text='Назад в главное меню', callback_data='back_menu')],
     ]
 )
@@ -44,15 +43,14 @@ story_notebook_kb = InlineKeyboardMarkup(
 
 instagram_marathon_kb = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text='Присоединиться', url='https://t.me/rudnyov')],
+        [InlineKeyboardButton(text='Присоединиться', url='https://t.me/+mX695lhKCAxhOGVi')],
         [InlineKeyboardButton(text='Назад', callback_data='back_list')],
         [InlineKeyboardButton(text='Назад в главное меню', callback_data='back_menu')]
     ]
 )
 
-ask_question_kb = InlineKeyboardMarkup(
+consultation_back_menu_kb = InlineKeyboardMarkup(
     inline_keyboard=[
-        [InlineKeyboardButton(text='Назад', callback_data='back_list')],
-        [InlineKeyboardButton(text='Назад в главное меню', callback_data='back_menu')],
+        [InlineKeyboardButton(text='Назад в главное меню', callback_data='back_menu')]
     ]
 )
